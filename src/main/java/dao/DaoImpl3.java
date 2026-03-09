2111b662e49ae3ca73863a9f4d738dacf52f8000
Farah Sahraoui <f.sahraoui8290@uca.ac.ma>
2026-03-09 21:41:32 +0100

    first commit

---
+++ src/main/java/dao/DaoImpl3.java
@@ -0,0 +1,12 @@
+package dao;
+
+import org.springframework.stereotype.Component;
+
+@Component("dao3")
+public class DaoImpl3 implements IDao {
+    @Override
+    public double getValue() {
+
+        return 250.0;
+    }
+}
